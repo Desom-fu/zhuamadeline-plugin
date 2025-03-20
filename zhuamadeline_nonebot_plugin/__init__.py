fd3be9ff584820cdcea8efbc2f198635003c8f68
Desom-fu <desom233@outlook.com>
2025-03-20 09:35:51 +0800

    保留无视冷却神权

--- zhuamadeline_nonebot_plugin/__init__.py
+++ zhuamadeline_nonebot_plugin/__init__.py
@@ -51,38 +51,38 @@ async def _():
 #         # 如果是，抛出 IgnoredException 来阻止命令的进一步处理
 #         raise IgnoredException("在 0:00 - 0:10 之间暂时禁止接收命令！")
     
-# # 运行前检查冷却时间
-# @run_preprocessor
-# async def check_cooldown(bot:Bot ,event: Event, matcher: Matcher):
-#     if isinstance(event, MessageEvent):  # 仅处理消息事件
-#         message_content = event.get_message().extract_plain_text()  # 提取纯文本内容
-#         if message_content.startswith(("。/", "./", "。、", ".、")): # 匹配这四个组合直接return
-#             return
-#         if message_content.startswith(("。", ".")):
-#             current_time = datetime.datetime.now()  #读取当前系统时间
-#             user_id = str(event.get_user_id())
-#             group_id = str(event.group_id)
-#             user_data = open_data(full_path)
-#             if user_data.get(user_id,{}).get('coolclear',False)== True:
-#                 next_time_r = current_time + datetime.timedelta(seconds=0)
-#                 user_data[str(user_id)]['next_time'] = next_time_r.strftime("%Y-%m-%d %H:%M:%S")
-#                 user_data[str(user_id)]['next_clock_time'] = next_time_r.strftime("%Y-%m-%d %H:%M:%S")
-#                 user_data[str(user_id)]['work_end_time'] = current_time.strftime("%Y-%m-%d %H:%M:%S")
-#                 save_data(full_path, user_data)
-#             if user_id in["2682786816", "121096913", "721396709", "1280785468"]: # 免冷却人群
-#                 return  # 不执行冷却时间检查，直接跳过
-#             current_time = time.time()
-#             cd_data = open_data(cd_path)
-#             coldtime = cd_data.get(user_id, {}).get("coldtime", 0)
-#             coldtime2 = cd_data.get('group',{}).get(group_id, {}).get("coldtime", 0)
-#             # 如果冷却时间未到，调用 emoji_like
-#             if current_time - coldtime2 < 1.2:
-#                 logger.info("群冷却中，无法发出消息")
-#                 raise IgnoredException("群冷却时间未到，请稍后再试。")
+# 运行前检查冷却时间
+@run_preprocessor
+async def check_cooldown(bot:Bot ,event: Event, matcher: Matcher):
+    if isinstance(event, MessageEvent):  # 仅处理消息事件
+        message_content = event.get_message().extract_plain_text()  # 提取纯文本内容
+        if message_content.startswith(("。/", "./", "。、", ".、")): # 匹配这四个组合直接return
+            return
+        if message_content.startswith(("。", ".")):
+            current_time = datetime.datetime.now()  #读取当前系统时间
+            user_id = str(event.get_user_id())
+            group_id = str(event.group_id)
+            user_data = open_data(full_path)
+            if user_data.get(user_id,{}).get('coolclear',False)== True:
+                next_time_r = current_time + datetime.timedelta(seconds=0)
+                user_data[str(user_id)]['next_time'] = next_time_r.strftime("%Y-%m-%d %H:%M:%S")
+                user_data[str(user_id)]['next_clock_time'] = next_time_r.strftime("%Y-%m-%d %H:%M:%S")
+                user_data[str(user_id)]['work_end_time'] = current_time.strftime("%Y-%m-%d %H:%M:%S")
+                save_data(full_path, user_data)
+            if user_id in["2682786816", "121096913", "721396709", "1280785468"]: # 免冷却人群
+                return  # 不执行冷却时间检查，直接跳过
+            # current_time = time.time()
+            # cd_data = open_data(cd_path)
+            # coldtime = cd_data.get(user_id, {}).get("coldtime", 0)
+            # coldtime2 = cd_data.get('group',{}).get(group_id, {}).get("coldtime", 0)
+            # # 如果冷却时间未到，调用 emoji_like
+            # if current_time - coldtime2 < 1.2:
+            #     logger.info("群冷却中，无法发出消息")
+            #     raise IgnoredException("群冷却时间未到，请稍后再试。")
             
-#             if current_time - coldtime < 1.5:
-#                 logger.info("冷却中，无法发出消息")
-#                 raise IgnoredException("冷却时间未到，请稍后再试。")
+            # if current_time - coldtime < 1.5:
+            #     logger.info("冷却中，无法发出消息")
+            #     raise IgnoredException("冷却时间未到，请稍后再试。")
 
 @event_postprocessor
 async def update_cooldown(event: Event):
