1e4355f84f36d13ac38bdca66c37dbe36eb13ad4
Desom-fu <desom233@outlook.com>
2025-03-30 12:43:30 +0800

    修改b2排版

--- zhuamadeline_nonebot_plugin/bet.py
+++ zhuamadeline_nonebot_plugin/bet.py
@@ -1285,7 +1285,7 @@ async def prop_demon_handle(bot: Bot, event: GroupMessageEvent, arg: Message = C
             msg += f"- 你退掉了一颗子弹，这颗子弹是：{bullet_type}\n"
         if not demon_data[group_id]['clip'] or all(b == 0 for b in demon_data[group_id]['clip']):
             demon_data[group_id]['clip'] = load()
-            msg += "- 子弹已耗尽，重新装填！\n"
+            msg += "- 子弹已耗尽，重新装填！\n\n"
             # 游戏轮数+1
             demon_data[group_id]['game_turn'] += 1
             # 获取死斗模式信息
