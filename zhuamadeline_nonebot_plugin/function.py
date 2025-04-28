a9991fd5785f336a21d9a7f2919cfe2762cd9f3f
Desom-fu <desom233@outlook.com>
2025-04-28 14:03:43 +0800

    优化数值2

--- zhuamadeline_nonebot_plugin/function.py
+++ zhuamadeline_nonebot_plugin/function.py
@@ -876,7 +876,7 @@ def spawn_boss(user_id, grade, boss_type=None):
 
 def spawn_world_boss():
     """生成世界Boss"""
-    hp = random.randint(200, 500)
+    hp = random.randint(300, 500)
     name = random.choice(boss_names["world"])
     
     boss_data = {
