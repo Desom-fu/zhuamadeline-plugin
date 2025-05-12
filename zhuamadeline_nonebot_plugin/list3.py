c9d04e6af7f60e4dbfe38f5abc7eecb9bdb992d8
Desom-fu <desom233@outlook.com>
2025-05-12 17:01:02 +0800

    让list里面的后缀都去掉了

--- zhuamadeline_nonebot_plugin/list3.py
+++ zhuamadeline_nonebot_plugin/list3.py
@@ -134,32 +134,27 @@ madeline_data3 = {
         '1':{
             'name': '鬼畜madeline',
             'description':
-            '玛德琳的喜怒哀乐',
-            'gif': True
+            '玛德琳的喜怒哀乐'
         },
         '2':{
             'name': 'madeline-boom',
             'description':
-            '或许是探求鲇鱼让玛德琳变成这样的',
-            'gif': True
+            '或许是探求鲇鱼让玛德琳变成这样的'
         },
         '3':{
             'name': '左右摇摆的badeline',
             'description':
-            'badeline也想起舞',
-            'gif': True
+            'badeline也想起舞'
         },
         '4':{
             'name': '旋转奶奶',
             'description':
-            '奶奶螺旋升天(物理)',
-            'gif': True
+            '奶奶螺旋升天(物理)'
         },
         '5':{
             'name': '星星眼madeline',
             'description':
-            '(✧ʌ✧)',
-            'jpg': True
+            '(✧ʌ✧)'
         },
         '6':{
             'name': '绿图',
@@ -184,8 +179,7 @@ madeline_data3 = {
         '10':{
             'name': '坐在椅子上的madeline',
             'description':
-            'madeline累了，好在椅子是可以坐的。',
-            'gif': True
+            'madeline累了，好在椅子是可以坐的。'
         },
         '11':{
             'name': '奶奶背爷爷',
@@ -195,8 +189,7 @@ madeline_data3 = {
         '12':{
             'name': '蓝弹球',
             'description':
-            '弹开madeline后使其充能，与普通的弹球没什么不同。',
-            'gif': True
+            '弹开madeline后使其充能，与普通的弹球没什么不同。'
         },
         '13':{
             'name': '被捆绑的madeline',
@@ -206,26 +199,22 @@ madeline_data3 = {
         '14':{
             'name': '绿色圣诞水晶之心',
             'description':
-            '产自购物中心，是圣诞节里轻松欢快的代表。',
-            'gif': True
+            '产自购物中心，是圣诞节里轻松欢快的代表。'
         },
         '15':{
             'name': '红色圣诞水晶之心',
             'description':
-            '产自午夜集市，是圣诞节的中流砥柱。',
-            'gif': True
+            '产自午夜集市，是圣诞节的中流砥柱。'
         },
         '16':{
             'name': '紫色圣诞水晶之心',
             'description':
-            '产自海滨广场，是圣诞节里最耀眼的明星。',
-            'gif': True
+            '产自海滨广场，是圣诞节里最耀眼的明星。'
         },
         '17':{
             'name': '黄色动量泡泡',
             'description':
-            '可以保留madeline横向动量的泡泡。',
-            'gif': True
+            '可以保留madeline横向动量的泡泡。'
         },
         '18':{
             'name': 'Fishing Badeline',
@@ -235,20 +224,17 @@ madeline_data3 = {
         '19':{
             'name': '水晶胖头鱼',
             'description':
-            '源自动量圣地，与普通的胖头鱼没什么区别。',
-            'gif': True
+            '源自动量圣地，与普通的胖头鱼没什么区别。'
         },
         '20':{
             'name': '粉弹球',
             'description':
-            '弹开madeline后使其获得双冲，很少有人知道它原本是绿色。',
-            'gif': True
+            '弹开madeline后使其获得双冲，很少有人知道它原本是绿色。'
         },
         '21':{
             'name': '飞草煤',
             'description':
-            '长着一对翅膀的草煤。出于未知的原因，翅膀似乎没有被煤球同化。',
-            'gif': True
+            '长着一对翅膀的草煤。出于未知的原因，翅膀似乎没有被煤球同化。'
         },
         '22':{
             'name': '山间回声',
@@ -263,70 +249,59 @@ madeline_data3 = {
         '24':{
             'name': '古代壁画',
             'description':
-            '原来是抽象的古代壁画啊，不知道的还以为是炼酒精在9-9翻车了呢',
-            'jpg': True
+            '原来是抽象的古代壁画啊，不知道的还以为是炼酒精在9-9翻车了呢'
         },
         '25':{
             'name': "再见I'm玛德琳",
             'description':
-            '拜拜咯您内！',
-            'jpg': True
+            '拜拜咯您内！'
         },
         '26':{
             'name': '玩蔚蓝玩的',
             'description':
-            '常年玩蔚蓝的人大多眼神锐利，极度自信，且智力逐渐增加，最后完全变成外星人，玩蔚蓝会重组身体结构，使得脑容量逐年上升，人一旦开始玩蔚蓝就说明这个人技术品味通通都是上等，这辈子肯定能扬名立万，玩蔚蓝的玩家具有强烈的责任感，对社会的建设起到决定性的作用。',
-            'jpg': True
+            '常年玩蔚蓝的人大多眼神锐利，极度自信，且智力逐渐增加，最后完全变成外星人，玩蔚蓝会重组身体结构，使得脑容量逐年上升，人一旦开始玩蔚蓝就说明这个人技术品味通通都是上等，这辈子肯定能扬名立万，玩蔚蓝的玩家具有强烈的责任感，对社会的建设起到决定性的作用。'
         },
         '27':{
             'name': '你的人格类型是ZKAD',
             'description':
-            '速通大神受我一拜',
-            'jpg': True
+            '速通大神受我一拜'
         },
         '28':{
             'name': '原来是SWAPSLUT',
             'description':
-            '还以为是Slut，原来是交换Slut',
-            'jpg': True
+            '还以为是Slut，原来是交换Slut'
         },
     },
     '2':{
         '1':{
             'name': '暗影弹球',
             'description':
-            '来自深渊的弹球，能使madeline获得暗影冲刺。',
-            'gif': True
+            '来自深渊的弹球，能使madeline获得暗影冲刺。'
         },
         '2':{
             'name': '紫色动量泡泡',
             'description':
-            '黄色动量泡泡的近亲，能保留的动量不再仅限于横向。',
-            'gif': True
+            '黄色动量泡泡的近亲，能保留的动量不再仅限于横向。'
         },
         '3':{
             'name': '反水母',
             'description':
-            '抓住反水母时，某些东西会变为反向，那么……祝你好运！',
-            'gif': True
+            '抓住反水母时，某些东西会变为反向，那么……祝你好运！'
         },
         '4':{
             'name': '无重力水母',
             'description':
-            '不受重力影响的神秘物种。',
-            'gif': True
+            '不受重力影响的神秘物种。'
         },
         '5':{
             'name': '白弹球',
             'description':
-            '弹开madeline后不为其充能，经常成群结队组成白弹球阵列。',
-            'gif': True
+            '弹开madeline后不为其充能，经常成群结队组成白弹球阵列。'
         },
         '6':{
             'name': '黄弹球',
             'description':
-            '虽然有个感叹号，但是也可以弹，不过只能弹一次。',
-            'gif': True
+            '虽然有个感叹号，但是也可以弹，不过只能弹一次。'
         },
         '7':{
             'name': '没有BUG!',
@@ -341,56 +316,47 @@ madeline_data3 = {
         '9':{
             'name': '弹球定义九宫格',
             'description':
-            '抓Madeline能把人创飞 抓Madeline也是弹球（确信）',
-            'jpg': True
+            '抓Madeline能把人创飞 抓Madeline也是弹球（确信）'
         },
         '10':{
             'name': '蔚蓝表情族谱',
             'description':
-            'catstare和frogeline两大家族共同统治着天下',
-            'jpg': True
+            'catstare和frogeline两大家族共同统治着天下'
         },
         '11':{
             'name': '发配到烈冕号',
             'description':
-            '发配发配，通通发配',
-            'jpg': True
+            '发配发配，通通发配'
         },
         '12':{
             'name': 'S属性大爆发',
             'description':
-            'Shattersong！',
-            'jpg': True
+            'Shattersong！'
         },
         '13':{
             'name': 'frogeline祈祷',
             'description':
-            '希望人没事',
-            'jpg': True
+            '希望人没事'
         },
         '14':{
             'name': '3A Demodash',
             'description':
-            '科学蔚蓝',
-            'jpg': True
+            '科学蔚蓝'
         },
         '15':{
             'name': '反重力活',
             'description':
-            '反重力来！反重力四面八方来！反重力铺天盖地来！反重力时时刻刻来！',
-            'jpg': True
+            '反重力来！反重力四面八方来！反重力铺天盖地来！反重力时时刻刻来！'
         },
         '16':{
             'name': '丝之鸽',
             'description':
-            '丝之歌……什么时候……',
-            'jpg': True
+            '丝之歌……什么时候……'
         },
         '17':{
             'name': '起星鱼了',
             'description':
-            '玩渊深玩的',
-            'jpg': True
+            '玩渊深玩的'
         },
         '18':{
             'name': '小鲨鱼',
@@ -420,14 +386,12 @@ madeline_data3 = {
         '23':{
             'name': '抹大拉',
             'description':
-            '抹大拉（mdl）也是玛德琳（mdl）',
-            'jpg': True
+            '抹大拉（mdl）也是玛德琳（mdl）'
         },
         '24':{
             'name': "Maya's Pumbers",
             'description':
-            '不知道好不好吃，但是肯定非常有弹力……',
-            'jpg': True
+            '不知道好不好吃，但是肯定非常有弹力……'
         },
         '25':{
             'name': '向导',
@@ -444,8 +408,7 @@ madeline_data3 = {
         '2':{
             'name': '月煤',
             'description':
-            '草煤的变异版本，长着小小的眼睛。',
-            'gif': True
+            '草煤的变异版本，长着小小的眼睛。'
         },
         '3':{
             'name': '电脑玛德琳',
@@ -455,26 +418,22 @@ madeline_data3 = {
         '4':{
             'name': 'CatTheo',
             'description':
-            '来自多元宇宙的邪恶版Theo。',
-            'gif': True
+            '来自多元宇宙的邪恶版Theo。'
         },
         '5':{
             'name': '猴莓',
             'description':
-            '非常好致幻蘑菇，爱来自cabob。',
-            'gif': True
+            '非常好致幻蘑菇，爱来自cabob。'
         },
         '6':{
             'name': '崩碎磁带',
             'description':
-            '崩碎之歌的源头。',
-            'gif': True
+            '崩碎之歌的源头。'
         },
         '7':{
             'name': '银草煤',
             'description':
-            '草煤的银色变种。可爱捏（？）',
-            'gif': True
+            '草煤的银色变种。可爱捏（？）'
         },
         '8':{
             'name': 'LV.SB有蔚批',
@@ -494,160 +453,134 @@ madeline_data3 = {
         '11':{
             'name': '背景开关',
             'description':
-            '神秘的开关，可以交换背景和墙体。',
-            'gif': True
+            '神秘的开关，可以交换背景和墙体。'
         },
         '12':{
             'name': '专业警卫无人机',
             'description':
-            '如果让他们看到你，你就死定了。',
-            'gif': True
+            '如果让他们看到你，你就死定了。'
         },
         '13':{
             'name': '三冲水晶',
             'description':
-            '吃下它，就能获得三段冲刺。',
-            'gif': True
+            '吃下它，就能获得三段冲刺。'
         },
         '14':{
             'name': '中子动力电池',
             'description':
-            'madeline在废弃的土星空间站找到了这个。',
-            'gif': True
+            'madeline在废弃的土星空间站找到了这个。'
         },
         '15':{
             'name': '暗影冲刺水晶',
             'description':
-            '吃下它的madeline下一次冲刺将会无敌。',
-            'gif': True
+            '吃下它的madeline下一次冲刺将会无敌。'
         },
         '16':{
             'name': '弹球泡泡',
             'description':
-            '可以提供弹球动力的一种泡泡。',
-            'gif': True
+            '可以提供弹球动力的一种泡泡。'
         },
         '17':{
             'name': '弹球炼狱评级表',
             'description':
-            '那么我宣布，“最佳弹球”大赛现在正式开始！',
-            'jpg': True
+            '那么我宣布，“最佳弹球”大赛现在正式开始！'
         },
         '18':{
             'name': '炼金的后果',
             'description':
-            '这个故事告诉我们一定要珍爱生命，远离炼金',
-            'jpg': True
+            '这个故事告诉我们一定要珍爱生命，远离炼金'
         },
         '19':{
             'name': '蔚蓝九宫格',
             'description':
-            '万物皆是蔚蓝。',
-            'jpg': True
+            '万物皆是蔚蓝。'
         },
         '20':{
             'name': "无cb金草莓",
             'description':
-            '寒冰科技月面基地的档案中最近发现了一些奇怪的异常，根据目前的观察结果，我们认为地图中有4个隐藏的草莓。然而，对一部分结构的细致分析表明，有可能存在第5个草莓。有意思……',
-            'gif': True
+            '寒冰科技月面基地的档案中最近发现了一些奇怪的异常，根据目前的观察结果，我们认为地图中有4个隐藏的草莓。然而，对一部分结构的细致分析表明，有可能存在第5个草莓。有意思……'
         },
     },
     '4':{
         '1':{
             'name': '小石德琳',
             'description':
-            '一只无意识的自由愉快的小石，听说守矢棋牌室好像搬到了塞莱斯特山上，便打算也来爬山，但是她赖床了。',
-            'gif': True     
+            '一只无意识的自由愉快的小石，听说守矢棋牌室好像搬到了塞莱斯特山上，便打算也来爬山，但是她赖床了。'     
         },
         '2':{
             'name': '神秘洗衣机',
             'description':
-            'awmc',
-            'gif': True     
+            'awmc'     
         },
         '3':{
             'name': '金草煤',
             'description':
-            '可能会发出尖锐的爆炸声',
-            'gif': True     
+            '可能会发出尖锐的爆炸声'     
         },
         '4':{
             'name': 'Salem',
             'description':
-            'Salem偏执而疯狂，在不知全貌的情况下盲目复仇，最后竟然将枪头指向了昔日好友Victor。',
-            'gif': True     
+            'Salem偏执而疯狂，在不知全貌的情况下盲目复仇，最后竟然将枪头指向了昔日好友Victor。'     
         },
         '5':{
             'name': 'Victor',
             'description':
-            'Victor平时没什么主见，但为了保护地球的来客，以生命为代价挡下了Salem的子弹。',
-            'gif': True     
+            'Victor平时没什么主见，但为了保护地球的来客，以生命为代价挡下了Salem的子弹。'     
         },
         '6':{
             'name': 'Evie',
             'description':
-            'Evie在登山过程中受伤，等待madeline的救援。',
-            'gif': True     
+            'Evie在登山过程中受伤，等待madeline的救援。'     
         },
         '7':{
             'name': '姜饼人',
             'description':
-            '糖果世界的npc，在圣诞节前夕向madeline发布寻找六颗宝石的任务。',
-            'gif': True     
+            '糖果世界的npc，在圣诞节前夕向madeline发布寻找六颗宝石的任务。'     
         },
         '8':{
             'name': 'Theia',
             'description':
-            '由于人们遗忘了他的神龛，Theia即将死去，好在madeline找到了失落的百合花。',
-            'gif': True     
+            '由于人们遗忘了他的神龛，Theia即将死去，好在madeline找到了失落的百合花。'     
         },
         '9':{
             'name': 'Sarsaparilla',
             'description':
-            '为了证明自己，Sarsaparilla踏上了一段冒险，并在异世界中遇见了madeline。',
-            'gif': True     
+            '为了证明自己，Sarsaparilla踏上了一段冒险，并在异世界中遇见了madeline。'     
         },
         '10':{
             'name': 'Astrixie',
             'description':
-            '健身中心的老板，成功的女商人，星球上最炙手可热的女王。',
-            'gif': True     
+            '健身中心的老板，成功的女商人，星球上最炙手可热的女王。'     
         },
         '11':{
             'name': 'RuPulsar',
             'description':
-            '在女孩们穿上第一双高跟鞋之前，她就已经在星球上做母亲了。',
-            'gif': True     
+            '在女孩们穿上第一双高跟鞋之前，她就已经在星球上做母亲了。'     
         },
         '12':{
             'name': 'Galaska',
             'description':
-            '她被雇来做室内装饰，希望能像星球上的其他人一样自信。',
-            'gif': True     
+            '她被雇来做室内装饰，希望能像星球上的其他人一样自信。'     
         },
         '12':{
             'name': 'Galaska',
             'description':
-            '她被雇来做室内装饰，希望能像星球上的其他人一样自信。',
-            'gif': True     
+            '她被雇来做室内装饰，希望能像星球上的其他人一样自信。'     
         },
         '13':{
             'name': 'Violebula',
             'description':
-            '她总是使用着这部只能连接到星球的手机进行异地恋。',
-            'gif': True     
+            '她总是使用着这部只能连接到星球的手机进行异地恋。'     
         },
         '14':{
             'name': 'Akershi',
             'description':
-            '在时限塔分崩离析后，Akershi被帝牙卢卡派到世界大穴收集散落的残片。',
-            'gif': True
+            '在时限塔分崩离析后，Akershi被帝牙卢卡派到世界大穴收集散落的残片。'
         },
         '15':{
             'name': '汤姆森博士',
             'description':
-            '正在对madeline尝试一种新的心理疗法。',
-            'gif': True
+            '正在对madeline尝试一种新的心理疗法。'
         },
         '16':{
             'name': '蜘蛛磁铁',
@@ -659,26 +592,22 @@ madeline_data3 = {
         '1':{
             'name': 'Celestin Goulvet',
             'description':
-            '（哎古小姐）别名Yann Pelote，一位住在一座山山脚下然后天天拿着画板画画的画家。（哎古大小姐）',
-            'gif': True     
+            '（哎古小姐）别名Yann Pelote，一位住在一座山山脚下然后天天拿着画板画画的画家。（哎古大小姐）'     
         },
         '2':{
             'name': 'Nikumaru Namo',
             'description':
-            '互联网书呆子，游走于边境的咖啡学者！就是我Nikumaru☆',
-            'gif': True     
+            '互联网书呆子，游走于边境的咖啡学者！就是我Nikumaru☆'     
         },
         '3':{
             'name': 'xiaozu-boom',
             'description':
-            '被气笑魔盒和气笑扑克气晕的小卒对着伯特按下了致死量的按钮',
-            'gif': True     
+            '被气笑魔盒和气笑扑克气晕的小卒对着伯特按下了致死量的按钮'     
         },
         '4':{
             'name': '霓虹彩虹草莓',
             'description':
-            '被霓虹灯光包裹之后，彩虹草莓散发出梦幻般的光泽。',
-            'gif': True     
+            '被霓虹灯光包裹之后，彩虹草莓散发出梦幻般的光泽。'     
         },
         '5':{
             'name': '天堂的总冠军',
@@ -693,26 +622,22 @@ madeline_data3 = {
         '7':{
             'name': 'ABuffZucchini',
             'description':
-            '虽是西葫芦却长得像仙人掌，Vivid Abyss足以让他青史留名。',
-            'gif': True     
+            '虽是西葫芦却长得像仙人掌，Vivid Abyss足以让他青史留名。'     
         },
         '8':{
             'name': 'Banana 23',
             'description':
-            '最擅长写散步图，记得去玩Banana Mountain。',
-            'gif': True     
+            '最擅长写散步图，记得去玩Banana Mountain。'     
         },
         '9':{
             'name': 'BossSauce',
             'description':
-            '作图十分有想法，不过你最好祈祷他不要随手放刺。',
-            'gif': True     
+            '作图十分有想法，不过你最好祈祷他不要随手放刺。'     
         },
         '10':{
             'name': 'Donker19',
             'description':
-            '他的创意从来不局限于gameplay，The Solaris System便是巅峰之作。',
-            'gif': True     
+            '他的创意从来不局限于gameplay，The Solaris System便是巅峰之作。'     
         },
         '11':{
             'name': '古迹守护者',
@@ -722,8 +647,7 @@ madeline_data3 = {
         '12':{
             'name': '赤骸星核',
             'description':
-            '绿辉石矿洞的裂渊深处，岩壁渗出暗红锈迹，如干涸的血脉指向这枚猩红海星状晶体。',
-            'gif': True     
+            '绿辉石矿洞的裂渊深处，岩壁渗出暗红锈迹，如干涸的血脉指向这枚猩红海星状晶体。'     
         },
     },
 }
